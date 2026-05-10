dfd19d566a5199425609a816f5175c78d78b97d5
zainab1-eng <zainabsiddiq2308@gmail.com>
2026-05-10 12:57:54 +0500

    main class code

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class Student:
+    name: str
+    age: int
+    cnic: str
+
+    def __str__(self):
+        return f"{self.name}, {self.age}, {self.cnic}"
+
+@dataclass
+class Class:
+    title: str
+    students: List[Student]
+
+    def __str__(self):
+        return f"{self.title}, {[st.name for st in self.students]}"
+
+
+zainab = Student(name="Zainab Siddiq", age=22, cnic="345768684756")
+sundas = Student(name="Sundas", age=22, cnic="375687487563")
+
+clss = Class(title="Software Engineering", students=[zainab, sundas])
+print(clss)
